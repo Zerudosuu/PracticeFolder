ab3cdb4d7b78077a582361a6053cb0360a1ef8b8
Zerudosuu <ronsalvador.m@gmail.com>
2024-01-05 08:27:47 +0800

    Displayed the product

--- React-ShoppingCenter/src/App.jsx
+++ React-ShoppingCenter/src/App.jsx
@@ -2,12 +2,37 @@ import NavBar from "./Navbar";
 import Hero from "./Hero";
 import ProductCard from "./ProductCard";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.escuelajs.co/api/v1/products");
+      const data = await response.json();
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <NavBar />
       <Hero />
-      <ProductCard />
+
+      {data.length > 0 ? (
+        <div className="ProductWrapper container">
+          {data.map((product) => (
+            <ProductCard key={product.id} data={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No product Found</h2>
+        </div>
+      )}
     </>
   );
 }
--- React-ShoppingCenter/src/ProductCard.jsx
+++ React-ShoppingCenter/src/ProductCard.jsx
@@ -1,17 +1,16 @@
 import "./style/ProductCard.scss";
 
 const ProductCard = (props) => {
+  const product = props.data;
+
+  console.log(product);
   const description =
     "iconic designs of Nike with the luxury and high-fashion aesthetic of Dior.";
   return (
     <div className="productCardContainer">
-      <img src="../public/download.jpg" />
-      <h1></h1>
-      <h4>
-        {description.length > 10
-          ? description.substring(0, 30).concat("..")
-          : description}
-      </h4>
+      <img src={product.images[0]} />
+      <h1>{product.title}</h1>
+      <h4>{product.description}</h4>
       <h5>Price</h5>
       <h6>Rating</h6>
     </div>
