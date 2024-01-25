7a99c34023415a44451946c8b3c122207f90b705
Ronald Salvador <ronsalvador.m@gmail.com>
2024-01-25 09:51:48 +0800

    Adding ShadCn playground for testing design

---
+++ Porfolio-react/ShadCNUI/ShadCN-playground/src/App.jsx
@@ -0,0 +1,15 @@
+import { AlertDemo } from "./ComponentsShadCn/Alert";
+import { Button } from "./components/ui/button";
+import { AlertDialogDemo } from "./ComponentsShadCn/AlertDialog";
+
+function App() {
+  return (
+    <>
+      <Button>Hey</Button>
+      <AlertDemo></AlertDemo>
+      <AlertDialogDemo></AlertDialogDemo>
+    </>
+  );
+}
+
+export default App;
