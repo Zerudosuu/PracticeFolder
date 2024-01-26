1b36559a696a09b2604f4d02f32f3bab38ca0341
Ronald Salvador <ronsalvador.m@gmail.com>
2024-01-26 20:41:43 +0800

    Creating Hero Section, NavBar done

--- Porfolio-react/Portfolio-JS/bank-modern-app/src/components/Navbar.jsx
+++ Porfolio-react/Portfolio-JS/bank-modern-app/src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  console.log(navLinks);
+  const [menuOpen, setMenu] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="logo" className="w-[124px] h-[32]" />
@@ -23,7 +24,33 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="sm:hidden flex flex-1 justify-end items-center"></div>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={menuOpen ? close : menu}
+          alt="menu"
+          className=" cursor-pointer w-[28px] h-[28px] object-contain"
+          onClick={() => setMenu((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            menuOpen ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+        >
+          <ul className=" list-none flex flex-col justify-center items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white `}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
