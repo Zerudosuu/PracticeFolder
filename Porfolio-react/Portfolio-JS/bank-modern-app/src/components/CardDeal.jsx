722120175facaeb50eb0899f59a6fe2e3147c6d1
Ronald Salvador <ronsalvador.m@gmail.com>
2024-01-28 19:12:51 +0800

    almost done with the website design. CTA section left

--- Porfolio-react/Portfolio-JS/bank-modern-app/src/components/CardDeal.jsx
+++ Porfolio-react/Portfolio-JS/bank-modern-app/src/components/CardDeal.jsx
@@ -1,5 +1,30 @@
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+import { card } from "../assets";
+import { layout, styles } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section id="product" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImgReverse}>
+      <img src={card} alt="bill" className="w-[100%] h-[100%] relative z-[5]" />
+
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
+  </section>
+);
 
 export default CardDeal;
