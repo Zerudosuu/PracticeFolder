9e4b22657991cd30e92fb88e2dadd3e3e08e6c97
Ronald Salvador <ronsalvador.m@gmail.com>
2024-02-11 21:19:35 +0800

    Applying SRP on Auth

--- Porfolio-react/DOTNET/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ Porfolio-react/DOTNET/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68653502515ca357d319a90d4d5ca021a7499133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+400fd8517b1eeb8494902d2e2abd17e76f92a282")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Porfolio-react/Portfolio-JS/Ecommerce-react/Ecommerce-react/src/pages/authenticationPage/Auth.jsx
+++ Porfolio-react/Portfolio-JS/Ecommerce-react/Ecommerce-react/src/pages/authenticationPage/Auth.jsx
@@ -1,33 +1,11 @@
-import { useState } from "react";
 import { auth, googleProvider } from "../../config/firebaseconfig";
-import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
+import "./Auth.css";
+import Signup from "./Signup";
 
 const Auth = () => {
-  const [user, setUser] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const signInWithPasswordandEmail = async (e) => {
-    e.preventDefault();
-
-    try {
-      const { username, email, password } = user;
-
-      // You can add validation here before proceeding with authentication
-
-      // Example of creating a user with email and password
-      await createUserWithEmailAndPassword(auth, email, password);
-
-      // Further logic or redirection can be added here
-
-      localStorage.setItem("user", user);
-      console.log("success!");
-    } catch (error) {
-      console.error("Error signing in with email and password:", error.message);
-    }
-  };
+  // TODO: Need to study about how to create collections and append the orders to it, with different users.
+  // ! IMPORTANT: This should be refactored as well as follow SRP
 
   const signInWithGoogle = async () => {
     try {
@@ -41,38 +19,15 @@ const Auth = () => {
       };
 
       console.log(userInfo);
-
-      // Further logic or redirection can be added here
     } catch (error) {
       console.error("Error signing in with Google:", error.message);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={signInWithPasswordandEmail}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={user.username}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
-        <button type="submit">Sign In with Email and Password</button>
-      </form>
-
-      <button onClick={signInWithGoogle}>Sign In with Google</button>
+    <div className="Form">
+      <Signup />
+      <button onClick={signInWithGoogle}>Google Sign in</button>
     </div>
   );
 };
